559ccd5ccbbef4f03dadb7352c582f066fa3c469
Nitendra <nitinjnumca09@gmail.com>
2020-07-30 23:06:51 +0800

    Updates

---
+++ precache-manifest.aeba9a4c29646a08bd94ca57b7d4b559.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc8d33bb4b3cafed70dc87583f69cb02",
+    "url": "/portfolio-app/index.html"
+  },
+  {
+    "revision": "fcacb891dc586d4ad0e4",
+    "url": "/portfolio-app/static/css/main.dbc3827c.chunk.css"
+  },
+  {
+    "revision": "734ed6fc3dd03ed77ca0",
+    "url": "/portfolio-app/static/js/2.ff59bdd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-app/static/js/2.ff59bdd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcacb891dc586d4ad0e4",
+    "url": "/portfolio-app/static/js/main.7a6890d0.chunk.js"
+  },
+  {
+    "revision": "32d263ee7bc5d1874f12",
+    "url": "/portfolio-app/static/js/runtime-main.bbc09db9.js"
+  }
+]);
